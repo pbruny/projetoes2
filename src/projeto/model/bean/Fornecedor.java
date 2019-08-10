5297e49c71b685681328d52ab446fe2797178e22
Paulo Lima <paulobruny@gmail.com>
2019-08-09 23:37:22 -0300

    Modelo de classe Fornecedor para o banco

---
+++ src/projeto/model/bean/Fornecedor.java
@@ -0,0 +1,25 @@
+package projeto.model.bean;
+
+public class Fornecedor {
+	
+	private long cnpj;
+	private String nome;
+	
+	public long getCnpj() {
+		return cnpj;
+	}
+	
+	public void setCnpj(long cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
