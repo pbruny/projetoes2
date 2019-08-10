e2b968d91e72765c9c7543b0d4e564f1aa6de9ce
Paulo Lima <paulobruny@gmail.com>
2019-08-09 23:39:25 -0300

    Modelo de classe pecas para o banco

---
+++ src/projeto/model/bean/Pecas.java
@@ -0,0 +1,27 @@
+package projeto.model.bean;
+
+public class Pecas {
+	
+	private int codigoPeca;
+	private String nome;
+	
+	
+	public int getCodigoPeca() {
+		return codigoPeca;
+	}
+	
+	public void setCodigoPeca(int codigoPeca) {
+		this.codigoPeca = codigoPeca;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
