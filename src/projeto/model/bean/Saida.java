18874ed0568dab7d674fbc9371f21a065e258a09
Paulo Lima <paulobruny@gmail.com>
2019-08-09 23:42:40 -0300

    Modelo de classe saida para o banco

---
+++ src/projeto/model/bean/Saida.java
@@ -0,0 +1,52 @@
+package projeto.model.bean;
+
+public class Saida {
+	
+	private long cpf;
+	private int codigoProduto;
+	private long qtd;
+	private Double valorTotal;
+	private int formaPag;
+	
+	public long getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+	
+	public int getCodigoProduto() {
+		return codigoProduto;
+	}
+	
+	public void setCodigoProduto(int codigoProduto) {
+		this.codigoProduto = codigoProduto;
+	}
+	
+	public long getQtd() {
+		return qtd;
+	}
+	
+	public void setQtd(long qtd) {
+		this.qtd = qtd;
+	}
+	
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+	
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+	public int getFormaPag() {
+		return formaPag;
+	}
+	
+	public void setFormaPag(int formaPag) {
+		this.formaPag = formaPag;
+	}
+	
+	
+}
